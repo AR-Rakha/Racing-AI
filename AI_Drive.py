3be7269a79cf9a57514b8a3a9a1be27f07452bb5
A.R.Rakha <NEXT16279@edu.nextkbh.dk>
2025-11-20 12:12:23 +0100

    Track og sidelæns friktion tilføjet

--- AI_Drive.py
+++ AI_Drive.py
@@ -1,6 +1,10 @@
 import pygame
 import math
 
+import numpy as np
+
+
+
 pygame.init()
 
 class bil:
@@ -12,8 +16,7 @@ class bil:
   def __str__(self):
     pass
 
-  def lagerUpdate(self):
-    pass
+  
 
 S_Width=1080
 
@@ -21,11 +24,20 @@ S_Height=1080
 
 screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 
+w, h = pygame.display.get_surface().get_size()
+
+
 run=True
 
 #car = pygame.Rect(300,300,50,50)
 car = pygame.image.load("Formula_1_car.png")
+car=pygame.transform.scale_by(car,0.5)
+track=pygame.image.load("Track.webp")
+track_scaled=pygame.transform.scale_by(track,1.5)
+track_rect=track_scaled.get_rect(center=(w/2,h/2))
 
+pos=np.array([0,0])
+vel=np.array([0,0])
 
 x_pos=0
 y_pos=0
@@ -41,25 +53,41 @@ angle_de_acc_strength=10
 
 border_bounce_multiplier=1
 
-air_resistance_multiplier=0.002
-
-air_resistance=1-air_resistance_multiplier
+air_resistance=1-(0.002)
 
 
-w, h = pygame.display.get_surface().get_size()
 
-#def getForwardVector():
+def getForwardVector(angle):
+  return np.array([math.sin(angle*math.pi/180),math.cos(angle*math.pi/180)])
+def getRightVector(angle):
+  return np.array([math.cos(angle*math.pi/180),-math.sin(angle*math.pi/180)])
 
+def grip(vel,angle,forward_friction_multiplier,sideways_friction_multiplier):
+  forward = getForwardVector(angle)
+  right = getRightVector(angle)
+
+  forward_comp = np.dot(vel, forward)
+  sideways_comp = np.dot(vel, right)
+
+  forward_force = -forward * forward_comp * forward_friction_multiplier
+  sideways_force = -right * sideways_comp * sideways_friction_multiplier
+
+  return forward_force + sideways_force
+  
+  
 
 while run:
   
   screen.fill((0,0,0,0.1))
 
   
+  screen.blit(track_scaled,track_rect)
+  
   car_rotated=pygame.transform.rotate(car,angle)
-  car_rect=car_rotated.get_rect(center=(x_pos,y_pos))
+  car_rect=car_rotated.get_rect(center=(pos[0],pos[1]))
   screen.blit(car_rotated,car_rect)
-  #pygame.draw.rect(screen,(255,0,0,0),car)
+
+  
 
   key= pygame.key.get_pressed()
   if key[pygame.K_a]:
@@ -68,32 +96,30 @@ while run:
     angle= angle-1
   
   if key[pygame.K_s]:
-    x_vel=x_vel+math.sin(angle*math.pi/180)*acc_multiplier
-    y_vel=y_vel+math.cos(angle*math.pi/180)*acc_multiplier
+    vel[0]=vel[0]+math.sin(angle*math.pi/180)*acc_multiplier
+    vel[1]=vel[1]+math.cos(angle*math.pi/180)*acc_multiplier
   elif key[pygame.K_w]:
-    x_vel=x_vel-math.sin(angle*math.pi/180)*acc_multiplier
-    y_vel=y_vel-math.cos(angle*math.pi/180)*acc_multiplier
+    vel[0]=vel[0]-math.sin(angle*math.pi/180)*acc_multiplier
+    vel[1]=vel[1]-math.cos(angle*math.pi/180)*acc_multiplier
 
-  x_pos=x_pos+x_vel
-  y_pos=y_pos+y_vel
+  pos=np.add(pos,vel)
 
-  if x_pos<0:
-    x_pos=0
-    x_vel=x_vel*-border_bounce_multiplier
-  if x_pos>w:
-    x_pos=w
-    x_vel=x_vel*-border_bounce_multiplier
+  if pos[0]<0:
+    pos[0]=0
+    vel[0]=vel[0]*-border_bounce_multiplier
+  if pos[0]>w:
+    pos[0]=w
+    vel[0]=vel[0]*-border_bounce_multiplier
 
-  if y_pos<0:
-    y_pos=0
-    y_vel=y_vel*-border_bounce_multiplier
-  elif y_pos>h:
-    y_pos=h
-    y_vel=y_vel*-border_bounce_multiplier
+  if pos[1]<0:
+    pos[1]=0
+    vel[1]=vel[1]*-border_bounce_multiplier
+  elif pos[1]>h:
+    pos[1]=h
+    vel[1]=vel[1]*-border_bounce_multiplier
 
-  x_vel=x_vel*air_resistance
-  y_vel=y_vel*air_resistance
-
+  #vel=np.multiply(vel,air_resistance)
+  vel=vel+grip(vel,angle,0.001,0.02)
 
   if key[pygame.K_ESCAPE]:
     run = False
